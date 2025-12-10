524a9c62246e1c4b2aa02719e471e6ea2c34afa2
sravan <sunnysravanssk@gmail.com>
2025-12-10 16:03:51 +0530

    Fix: make STATIC_URL absolute ('/static/') so {% static %} resolves correctly

--- fitmind/settings.py
+++ fitmind/settings.py
@@ -115,7 +115,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+# When DEBUG is True Django's dev server will serve static files
+# from app `static/` folders. STATIC_URL should be an absolute
+# URL path so `{% static '...' %}` produces an absolute path.
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
